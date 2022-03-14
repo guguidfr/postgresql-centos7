c2986308c2efe6bb156c2760b73717442c1d1ee9
José Daniel <jdrayman@hotmail.com>
2022-03-14 12:55:21 +0100

    Update and rename datos1.sql to datos-de-ejemplo.sql

---
+++ datos-de-ejemplo.sql
@@ -0,0 +1,5 @@
+\c /*nombre de la base de datos en la que quieras crear la tabla*/
+create table /*nombre de la tabla que quieras crear*/ (
+	message varchar(255) not null
+);
+insert into tabladeprueba values('/*escribe un mensaje aquí*/');
--- datos1.sql
+++
@@ -1,5 +0,0 @@
-\c basededatos1
-create table tabladeprueba (
-	message varchar(255) not null
-);
-insert into tabladeprueba values('Ta wapo el ansible');
