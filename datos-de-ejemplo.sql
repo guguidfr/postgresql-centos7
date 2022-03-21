961cecfcf3aa0e6e6c2ed42b9d8601265da3841a
José Daniel <jdrayman@hotmail.com>
2022-03-21 12:29:07 +0100

    Update datos-de-ejemplo.sql

--- datos-de-ejemplo.sql
+++ datos-de-ejemplo.sql
@@ -1,5 +1,5 @@
 \c /*nombre de la base de datos en la que quieras crear la tabla*/
-create table /*nombre de la tabla que quieras crear*/ (
+create table tabla1 (
 	message varchar(255) not null
 );
-insert into /*nombre de la tabla que acabas de crear*/ values('escribe un mensaje aquí');
+insert into tabla1 values('Hello World!');
