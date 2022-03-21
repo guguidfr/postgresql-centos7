0d861c97a7162b70be6b2339e6a8ebf067c7fc7f
José Daniel <jdrayman@hotmail.com>
2022-03-21 12:29:47 +0100

    Update datos-de-ejemplo.sql

--- datos-de-ejemplo.sql
+++ datos-de-ejemplo.sql
@@ -1,4 +1,4 @@
-\c /*nombre de la base de datos en la que quieras crear la tabla*/
+\c bdprueba1 /*nombre de la base de datos en la que quieras crear la tabla*/
 create table tabla1 (
 	message varchar(255) not null
 );
